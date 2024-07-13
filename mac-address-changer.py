5c7f9a917b0e913b226be8c44919fdb251158451
my-holber <159010114+my-holber@users.noreply.github.com>
2024-07-13 18:21:14 +0200

    Update mac-address-changer.py

--- mac-address-changer.py
+++ mac-address-changer.py
@@ -10,12 +10,14 @@ def get_arguments():
     Checks for necessary arguments and displays an error if they are missing.
     """
     parser = optparse.OptionParser()
-    parser.add_option('-i', '--interface', dest='interface', help='Interface to ch>    parser.add_option('-m', '--mac', dest='new_mac', help='New MAC address')
+    parser.add_option('-i', '--interface', dest='interface', help='Interface to change its MAC address')
+    parser.add_option('-m', '--mac', dest='new_mac', help='New MAC address')
     (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info.")
     elif not options.new_mac:
-        parser.error("[-] Please specify a new MAC address, use --help for more in>    return options
+        parser.error("[-] Please specify a new MAC address, use --help for more info.")
+    return options
 
 
 def change_mac(interface, new_mac):
@@ -23,7 +25,9 @@ def change_mac(interface, new_mac):
     Changes the MAC address of the specified network interface.
     """
     print("[+] Changing MAC address for", interface, "to", new_mac)
-    subprocess.run(['ifconfig', interface, 'down'], capture_output=True, text=True)    subprocess.run(['ifconfig', interface, 'hw', 'ether', new_mac], capture_output>    subprocess.run(['ifconfig', interface, 'up'], capture_output=True, text=True)
+    subprocess.run(['ifconfig', interface, 'down'], capture_output=True, text=True)
+    subprocess.run(['ifconfig', interface, 'hw', 'ether', new_mac], capture_output=True, text=True)
+    subprocess.run(['ifconfig', interface, 'up'], capture_output=True, text=True)
 
 '''Get command-line arguments'''
 options = get_arguments()
